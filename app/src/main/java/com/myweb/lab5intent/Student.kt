2c378a41d4bf9cbcc465f986056fb99bd3cccc73
seth-setthaphong <70099308+sethhyyy@users.noreply.github.com>
2020-08-31 16:23:38 +0700

    ส่งงาน Lab5 Sec3

---
+++ app/src/main/java/com/myweb/lab5intent/Student.kt
@@ -0,0 +1,7 @@
+package com.myweb.lab5intent
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+@Parcelize
+data class Student (val id:String, val name: String, val age: Int) : Parcelable {
+
+}
